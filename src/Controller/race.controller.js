d59efaf2201c7d926f02ca69a59d0dea74313543
Ricardo Rodriguez <rjrodriguezalvarez97@gmail.com>
2021-10-25 13:40:57 +0200

    Route, controller and a little bug fix

---
+++ src/Controller/race.controller.js
@@ -0,0 +1,30 @@
+const RaceService = require('../Service/RaceService');
+const CustomError = require('./CustomError');
+
+const raceService = new RaceService();
+module.exports = {
+  async getRaceRanking(req, res) {
+    try {
+      const { name } = req.query;
+      if (!name) {
+        throw new CustomError({
+          code: 400,
+          message: 'Missing race name on query parameter'
+        });
+      }
+      const race = await raceService.getRaceByName(name);
+      if (!race) {
+        throw new CustomError({
+          code: 404,
+          message: 'Race not found'
+        });
+      }
+      const raceRanking = await raceService.getRaceRanking(race);
+      return res.json(raceRanking);
+      // if (docs.length === 0) throw Error('Import failed. Try again later');
+      // res.json({ message: 'Data imported succesfully', docs });
+    } catch (err) {
+      return CustomError.handleError(err, res);
+    }
+  }
+};
--- src/Routes/router.js
+++ src/Routes/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { importData, exportData } = require('../Controller/import.controller');
+const { getRaceRanking } = require('../Controller/race.controller');
 
 module.exports = () => {
   const router = express.Router();
 
   router.post('/import', importData);
   router.get('/export', exportData);
+
+  router.get('/ranking/race', getRaceRanking);
   return router;
 };
